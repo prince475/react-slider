c2822bf2cfa7c0af5ce7386aa919e517d1306c9a
prince475 <danielmwai475@gmail.com>
2023-05-21 02:07:32 +0300

    Adding functionality to the page, from the slide to the arrows.

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SliderData } from './SliderData'
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
 
-export default function ImageSlider() {
+export default function ImageSlider({ slides }) {
+
+    const [current, setCurrent] = useState(0)
+
+    const length = slides.length
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1)
+    }
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1 )
+    }
+
+    if(!Array.isArray(slides) || slides.length <= 0) {
+        return null
+    }
+
   return(
-    <>
+    <section className='slider'>
+
+    <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
+    <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
+
+
     {SliderData.map((slide, index) => {
-        return <img src={slide.image} alt="career images"/>
+        return (
+            <div className={index === current ? 'slide active' : 'slide'} key={index}>
+              {index === current && ( <img src={slide.image} alt="career images" className='image'/>)}
+            </div>
+        )
     })}
-    </>
+    </section>
   )
 }
 
